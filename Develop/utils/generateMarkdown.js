0c721c67d696503e39b9b0125c5e1d88f768bef3
prayushapandey04 <prayusha.pandey4@gmail.com>
2023-10-20 16:05:23 -0500

    added function for licensing badges and links

--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -1,13 +1,38 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
-
-// return URL string to license badge link (it is somewhere in github
-//documentation)
+function renderLicenseBadge(license) {
+  if (license === 'Creative Commons') {
+    return '[![License: CC0-1.0](https://licensebuttons.net/l/zero/1.0/80x15.png)](http://creativecommons.org/publicdomain/zero/1.0/)';
+  } else if (license === 'Boost'){
+    return '[![License](https://img.shields.io/badge/License-Boost_1.0-lightblue.svg)](https://www.boost.org/LICENSE_1_0.txt)';
+  } else if (license === 'Apache 2.0') {
+    return '[![License](https://img.shields.io/badge/License-Apache_2.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)';
+  } else if (license === 'MIT') {
+    return '[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)';
+  } else if (license === 'IBM') {
+    return '[![License: IPL 1.0](https://img.shields.io/badge/License-IPL_1.0-blue.svg)](https://opensource.org/licenses/IPL-1.0)';
+  } else {
+    return '';
+  }
+}
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
+function renderLicenseLink(license) {
+  if (license === 'Creative Commons') {
+    return 'https://creativecommons.org/publicdomain/zero/1.0/deed.en';
+  } else if (license === 'Boost') {
+    return 'https://www.boost.org/LICENSE_1_0.txt';
+  } else if (license === "Apache 2.0") {
+    return 'https://www.apache.org/licenses/LICENSE-2.0';
+  } else if (license === 'MIT') {
+    return 'https://opensource.org/license/mit/';
+  } else if (license === 'IBM') {
+    return 'https://www.ibm.com/about/software-licensing/us-en';
+  } else {
+    return '';
+  }
+}
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
